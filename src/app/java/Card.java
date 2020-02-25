312c6db9450f829618b918d1a47e60783885f14d
NataliaRajska <natalia.rajskaa@gmail.com>
2020-02-25 13:21:18 +0100

    Add Card class

--- src/app/java/Card.java
+++ src/app/java/Card.java
@@ -1,4 +1,31 @@
 package app.java;
 
 public class Card {
+    private String name;
+    private int friendless;
+    private int happiness;
+    private int loyalty;
+
+    public Card(String name, int friendless, int happiness, int loyalty) {
+        this.name = name;
+        this.friendless = friendless;
+        this.happiness = happiness;
+        this.loyalty = loyalty;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getFriendless() {
+        return friendless;
+    }
+
+    public int getHappiness() {
+        return happiness;
+    }
+
+    public int getLoyalty() {
+        return loyalty;
+    }
 }
